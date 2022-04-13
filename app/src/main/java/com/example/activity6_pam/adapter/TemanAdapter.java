21ce221c015b701f02a1ab152d9be485fa6ec5ef
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2022-04-13 13:38:07 +0700

    Activity 7

--- app/src/main/java/com/example/activity6_pam/adapter/TemanAdapter.java
+++ app/src/main/java/com/example/activity6_pam/adapter/TemanAdapter.java
@@ -1,19 +1,28 @@
 package com.example.activity6_pam.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.activity6_pam.EditTeman;
+import com.example.activity6_pam.MainActivity;
 import com.example.activity6_pam.R;
+import com.example.activity6_pam.database.DBController;
 import com.example.activity6_pam.database.Teman;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
     private ArrayList<Teman> listData;
@@ -22,28 +31,63 @@ public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHol
     {
         this.listData = listData;
     }
+    private Context control;
 
     @Override
-    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    public TemanAdapter.TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
         LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
         View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
-
+        control = parent.getContext();
         return new TemanViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position)
+    public void onBindViewHolder(@NonNull TemanAdapter.TemanViewHolder holder, int position)
     {
-        String nm, tlp;
+        String nm, tlp, id;
 
+        id = listData.get(position).getId();
         nm = listData.get(position).getNama();
         tlp = listData.get(position).getTelpon();
+        DBController db = new DBController(control);
 
         holder.namaTxt.setTextColor(Color.BLUE);
-        holder.namaTxt.setTextSize(20);
+        holder.namaTxt.setTextSize(30);
         holder.namaTxt.setText(nm);
         holder.telponTxt.setText(tlp);
+
+        holder.cardku.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(control, holder.cardku);
+                popupMenu.inflate(R.menu.popup_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem Item) {
+                        switch (Item.getItemId()){
+                            case R.id.mnEdit:
+                                Intent i = new Intent(control, EditTeman.class);
+                                i.putExtra("id",id);
+                                i.putExtra("nama",nm);
+                                i.putExtra("telpon",tlp);
+                                control.startActivity(i);
+                                break;
+                            case R.id.mnHapus:
+                                HashMap<String,String > values = new HashMap<>();
+                                values.put("id",id);
+                                db.DeleteData(values);
+                                Intent j = new Intent(control, MainActivity.class);
+                                control.startActivity(j);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                return false;
+            }
+        });
     }
 
     @Override
